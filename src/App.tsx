02c7295cfbe8f4bd5a4e45fd21825461dea57c63
Saulo-Felipe <saulofelipe234567@gmail.com>
2023-08-31 10:05:39 -0300

    fix card error

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
 import Card from './components/Card';
 import Player from './components/Player';
 
 const players = [
-   {
-     name: 'Player 1',
-     pot: 100,
-     cards: [new Card({ suit: 'copas', rank: '2' })],
-// Adicione as cartas do jogador aqui
-   }]
+  {
+    name: 'Player 1',
+    pot: 100,
+    cards: [],
+  }
+]
 
 const App = () => {
   return (
     <div>
       <Card suit="copas" rank="2" />
       <Card suit="copas" rank="k" />
+      <Card suit="copas" rank="3" />
     </div>
   );
 };
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-
+import { ReactNode } from "react";
 // Defina o tipo das props do componente
 type CardProps = {
   suit: string; // naipe
   rank: string; // numero ou nome
 };
 
-const Card: React.FC<CardProps> = ({ suit, rank }) => {
-  const urlImage = `../assets/${suit}/${suit}_${rank}.png`; // caminho pode precisar ser corrigido
+function Card({ suit, rank }: CardProps): ReactNode {
+  const urlImage = `../${suit}/${suit}_${rank}.png`; // caminho pode precisar ser corrigido
 
   return (
     <div>
       <img src={urlImage} alt={`Carta ${rank} de ${suit}`} />
     </div>
   );
-};
+}
 
 export default Card;
