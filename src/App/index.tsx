c1ffa702604f1f1704d7a5ab5964980e9a52bfa1
Saulo-Felipe <saulofelipe234567@gmail.com>
2023-09-05 11:34:33 -0300

    alterando app.tsx, adicionando sass, criando mesa

---
+++ src/App/index.tsx
@@ -0,0 +1,49 @@
+import styles from "./style.module.scss";
+
+
+export function App() {
+   return (
+      <div className={styles.table}>
+         
+      </div>
+   )
+}
+
+
+
+
+
+
+
+
+
+
+
+// import { Player } from './components/Player';
+// import { createDeck, dealTwoCards } from "./helpers/BaralhoDealer";
+
+// // const players = [
+// //    {
+// //       name: 'Player 1',
+// //       pot: 100,
+// //       cards: [],
+// //    }
+// // ]
+
+// const App = () => {
+//    const baralho = createDeck();
+   
+//    return (
+//       <>
+//          <div>
+//             <Player name={'Joao Sorrisao'} pot={0} cards={dealTwoCards(baralho)} />
+//             <Player name={'Sorrisao'} pot={5000} cards={dealTwoCards(baralho)} />
+//          </div>
+//       </>
+
+
+//    );
+// }
+
+
+// export default App;
