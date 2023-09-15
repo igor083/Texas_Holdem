bad6ffb98f4a7dcc4c7134f3748d46ef20136454
Saulo-Felipe <saulofelipe234567@gmail.com>
2023-09-15 16:26:57 -0300

    lobby completo

---
+++ client/src/pages/Room/Game.tsx
@@ -0,0 +1,8 @@
+
+
+export function Game() {
+
+  return (
+    <div>Ola</div>
+  )
+}
--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import cors from "cors";
-import { Player, Room, PlayerState } from "./src/types/Types";
+import { Room, PlayerState } from "./src/types/Types";
 
 const app = express();
 const httpServer = createServer(app);
@@ -32,6 +32,7 @@ function deletePlayer(playerId: string) {
   for (const room of rooms) {
     for (const i in room.players) {
       if (room.players[Number(i)].id === playerId) {
+        console.log("[delete player]: ", room.players[Number(i)]);
         room.players.splice(Number(i), 1);
       }
     }
@@ -53,15 +54,26 @@ app.use(cors({
   origin: "http://localhost:5173"
 }));
 
+
+setInterval(() => {
+  console.log("[all players]: ", rooms.map(room => room.players));
+}, 2000);
+
 io.on("connection", socket => {
   console.log("Conected!");
 
   socket.on("disconnect", () => {
     const room = getRoomByPlayerId(socket.id);    
+
     deletePlayer(socket.id);
 
+    
     if (room) {
-      io.to(room.id).emit("update-players", room.players);
+      if (room?.ownerId === socket.id && room.players.length > 0) {
+        room.ownerId = room.players[0].id;
+        io.to(room.id).emit("[lobby] update-owner", room.ownerId);
+      }
+      io.to(room.id).emit("[lobby] update-players", room.players);
     }
   });
 
@@ -81,8 +93,8 @@ io.on("connection", socket => {
 
         socket.join(room.id);
 
-        socket.broadcast.to(room.id).emit("update-players", room.players);
-        socket.emit("initial-data", room);
+        socket.broadcast.to(room.id).emit("[lobby] update-players", room.players);
+        socket.emit("[lobby] initial-data", room);
       }
     })
 
